f0e4877af41a3e03c5f5bd2329fabfd27a32e237
Olukayode <olukayodeshokunbi@gmail.com>
2021-02-18 08:36:28 -0500

    Designed landing page with button that links to main page

---
+++ src/Components/LandingPage/landingPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./landingPage.css";
+
+function LandingPage() {
+  return (
+    <div className="landing">
+      <div className="landing-text-block">
+        <h1>SONGSPEDIA</h1>
+        <h3>...you search, ...we find, ...its all about music!</h3>
+        <button className="btn">Continue</button>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import LandingPage from "./Components/LandingPage/landingPage";
+
+ReactDOM.render(<LandingPage />, document.getElementById("root"));
