f0b78f7f2429bf112ec5bc15c6130d2dbe40fd6a
Olukayode <olukayodeshokunbi@gmail.com>
2021-02-18 23:09:55 -0500

    Modified GetAllSongs from function component to class component with state passed response data from fetch operation

--- src/Components/SongFetcher/songFetch.js
+++ src/Components/SongFetcher/songFetch.js
@@ -1,16 +1,51 @@
+import React, { Component } from "react";
 import axios from "axios";
 
-function GetAllSongs() {
-  axios
-    .get("http://www.devcodecampmusiclibrary.com/api/music")
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+class GetAllSongs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { songsData: [] };
+  }
 
-  return null;
+  componentDidCatch() {
+    axios
+      .get("http://www.devcodecampmusiclibrary.com/api/music")
+      .then((response) => {
+        const songsData = response.data;
+        this.setState({ songsData: songsData });
+      });
+  }
+
+  render() {
+    return (
+      <table>
+        {this.state.songsData.map((songRecord) => {
+          return (
+            <fragment>
+              <tr>{songRecord.id}</tr>
+              <tr>{songRecord.title}</tr>
+              <tr>{songRecord.album}</tr>
+              <tr>{songRecord.artist}</tr>
+              <tr>{songRecord.genre}</tr>
+              <tr>{songRecord.releaseDate}</tr>
+            </fragment>
+          );
+        })}
+      </table>
+    );
+  }
 }
 
 export default GetAllSongs;
+
+// fetchSongs() {
+//   axios
+//     .get("http://www.devcodecampmusiclibrary.com/api/music")
+//     .then((response) => {
+//       console.log(response.data);
+//       this.setState({ songsData: response.data });
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// }
