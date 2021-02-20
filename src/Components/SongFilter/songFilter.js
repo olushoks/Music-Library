94f4f068f1f17c999b5f70aba21bcb9a54369a6e
Olukayode <olukayodeshokunbi@gmail.com>
2021-02-20 03:19:00 -0500

    Created Filter class component to get filter criteria and filter value

---
+++ src/Components/SongFilter/songFilter.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./songFilter.css";
+
+class Filter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterCriteria: "",
+      filterValue: "",
+    };
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
