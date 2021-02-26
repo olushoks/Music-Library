d49d5eb356a6d3ba6eca19af7b0f9c8c4747f9d7
Olukayode <olukayodeshokunbi@gmail.com>
2021-02-25 22:16:57 -0500

    Changed radio buttons to regular clickable buttons

--- src/Components/MoreActions/moreActions.js
+++ src/Components/MoreActions/moreActions.js
@@ -4,58 +4,82 @@ import "./moreActions.css";
 class MoreAction extends Component {
   constructor(props) {
     super(props);
-    this.state = { action: undefined };
+    this.state = {};
   }
 
   getAction = (evt) => {
-    this.setState({ action: evt.target.value });
-    alert(`You want to ${this.state.action}`);
-    console.log(`${evt.target.value}`);
+    // this.setState({ action: evt.target.value });
+    const action = evt.target.value;
+    // alert(`You want to ${action}`);
+    console.log(`${action}`);
+    this.displayActionForm(action);
+  };
+
+  displayActionForm = (action) => {
+    if (action === "get") {
+      return (
+        <form>
+          <label>Get Song By ID:</label>
+          <input type="number"></input>
+        </form>
+      );
+    }
+
+    if (action === "add") {
+      return null;
+    }
+
+    if (action === "edit") {
+      return null;
+    }
+
+    if (action === "delete") {
+      return null;
+    }
+  };
+
+  // Radio Button Formm
+  actionButtons = () => {
+    return (
+      <div>
+        <button value="get" onClick={this.displayActionForm}>
+          Get Song By ID
+        </button>
+        <button value="add" onClick={this.displayActionForm}>
+          ADD NEW SONG
+        </button>
+        <button value="edit" onClick={this.displayActionForm}>
+          EDIT SONG
+        </button>
+        <button value="delete" onClick={this.displayActionForm}>
+          DELETE SONG
+        </button>
+      </div>
+      /*
+      <form onChange={this.getAction}>
+        <label>
+          <input type="radio" name="action" value="get"></input>
+          Get Song By ID
+        </label>
+        <label>
+          <input type="radio" name="action" value="add"></input>
+          Add New Song
+        </label>
+        <label>
+          <input type="radio" name="action" value="edit"></input>
+          Edit Song Detail
+        </label>
+        <label>
+          <input type="radio" name="action" value="delete"></input>
+          Delete Song
+        </label>
+      </form>
+      */
+    );
   };
 
   render() {
-    return (
-      <div>
-        <form onChange={this.getAction}>
-          <label>
-            Get Song By ID
-            <input
-              type="radio"
-              name="action"
-              value="get"
-              //   onClick={this.getAction}
-            ></input>
-          </label>
-          <label>
-            Add New Song
-            <input
-              type="radio"
-              name="action"
-              value="add"
-              //   onClick={this.getAction}
-            ></input>
-          </label>
-          <label>
-            Edit Song Detail
-            <input
-              type="radio"
-              name="action"
-              value="edit"
-              //   onClick={this.getAction}
-            ></input>
-          </label>
-          <label>
-            Delete Song
-            <input
-              type="radio"
-              name="action"
-              value="delete"
-              //   onClick={this.getAction}
-            ></input>
-          </label>
-        </form>
-      </div>
-    );
+    return <div>{this.actionButtons()}</div>;
   }
 }
 
