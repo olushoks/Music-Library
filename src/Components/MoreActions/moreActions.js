7ac1852588a33b63ecfee56d3936a5a3b1eac9ea
Olukayode <olukayodeshokunbi@gmail.com>
2021-02-25 19:48:07 -0500

    Created radio buttons for user to select action: GET, ADD, EDIT OR DELETE

--- src/Components/MoreActions/moreActions.js
+++ src/Components/MoreActions/moreActions.js
@@ -1,17 +1,59 @@
-import { ReactComponent } from "react";
 import React, { Component } from "react";
 import "./moreActions.css";
 
 class MoreAction extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { action: undefined };
   }
+
+  getAction = (evt) => {
+    this.setState({ action: evt.target.value });
+    alert(`You want to ${this.state.action}`);
+    console.log(`${evt.target.value}`);
+  };
 
   render() {
     return (
       <div>
-        <form></form>
+        <form onChange={this.getAction}>
+          <label>
+            Get Song By ID
+            <input
+              type="radio"
+              name="action"
+              value="get"
+              //   onClick={this.getAction}
+            ></input>
+          </label>
+          <label>
+            Add New Song
+            <input
+              type="radio"
+              name="action"
+              value="add"
+              //   onClick={this.getAction}
+            ></input>
+          </label>
+          <label>
+            Edit Song Detail
+            <input
+              type="radio"
+              name="action"
+              value="edit"
+              //   onClick={this.getAction}
+            ></input>
+          </label>
+          <label>
+            Delete Song
+            <input
+              type="radio"
+              name="action"
+              value="delete"
+              //   onClick={this.getAction}
+            ></input>
+          </label>
+        </form>
       </div>
     );
   }
