37d9973466ef22e117cc9473ee36849e8a2b8327
Olukayode <olukayodeshokunbi@gmail.com>
2021-05-27 09:12:38 -0400

    Changed the sstyling and positioning of the AddEditForm

--- front-end/src/Components/MoreActions/AddEditForm.js
+++ front-end/src/Components/MoreActions/AddEditForm.js
@@ -7,6 +7,7 @@ const AddEditForm = ({ action, song, submitForm, closeForm }) => {
     ...song,
   });
   const [alert, setAlert] = useState("");
+  const [successErrorMsg, setSuccessErrorMsg] = useState(null);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -17,9 +18,11 @@ const AddEditForm = ({ action, song, submitForm, closeForm }) => {
     const { title, album, artist, genre, releaseDate } = songInfo;
     e.preventDefault();
     if (!title || !album || !artist || !genre || !releaseDate) {
+      setSuccessErrorMsg("error");
       handleAlert(setAlert, "Empty values not allowed");
     } else {
       submitForm(songInfo);
+      setSuccessErrorMsg("success");
       handleAlert(
         setAlert,
         `Song succesfully ${action === "Edit" ? "edited" : "added"}`
@@ -36,67 +39,74 @@ const AddEditForm = ({ action, song, submitForm, closeForm }) => {
 
   return (
     <div className="form-overlay">
-      <form className="" onSubmit={handleSubmit}>
-        <h4>
-          {action} Song
-          <i
-            className="close-form-button fas fa-times-circle"
-            onClick={closeForm}
-          ></i>
-        </h4>
-        <p className="">{alert}</p>
-
-        <label className="more-label">
-          Song Title
-          <input
-            className="more-input"
-            type="text"
-            name="title"
-            value={songInfo.title}
-            onChange={handleChange}
-          ></input>
-        </label>
-        <label className="more-label">
-          Album
-          <input
-            className="more-input"
-            type="text"
-            name="album"
-            value={songInfo.album}
-            onChange={handleChange}
-          ></input>
-        </label>
-        <label className="more-label">
-          Artist
-          <input
-            className="more-input"
-            type="text"
-            name="artist"
-            value={songInfo.artist}
-            onChange={handleChange}
-          ></input>
-        </label>
-        <label className="more-label">
-          Genre
-          <input
-            className="more-input"
-            type="text"
-            name="genre"
-            value={songInfo.genre}
-            onChange={handleChange}
-          ></input>
-        </label>
-        <label className="more-label">
-          Release Date
-          <input
-            className="more-input"
-            type="text"
-            name="releaseDate"
-            value={songInfo.releaseDate}
-            onChange={handleChange}
-          ></input>
-        </label>
-        <input className="submit" type="submit" value="submit"></input>
+      <form className="more-form" onSubmit={handleSubmit}>
+        <i
+          className="close-form-button fas fa-times-circle"
+          onClick={closeForm}
+        ></i>
+        <h4>{action} Song</h4>
+        <div className="input-section">
+          {alert && (
+            <p
+              className={`${
+                alert && successErrorMsg === "success" ? "success" : "error"
+              }`}
+            >
+              {alert}
+            </p>
+          )}
+          <label className="more-label">
+            Song Title
+            <input
+              className="more-input"
+              type="text"
+              name="title"
+              value={songInfo.title}
+              onChange={handleChange}
+            ></input>
+          </label>
+          <label className="more-label">
+            Album
+            <input
+              className="more-input"
+              type="text"
+              name="album"
+              value={songInfo.album}
+              onChange={handleChange}
+            ></input>
+          </label>
+          <label className="more-label">
+            Artist
+            <input
+              className="more-input"
+              type="text"
+              name="artist"
+              value={songInfo.artist}
+              onChange={handleChange}
+            ></input>
+          </label>
+          <label className="more-label">
+            Genre
+            <input
+              className="more-input"
+              type="text"
+              name="genre"
+              value={songInfo.genre}
+              onChange={handleChange}
+            ></input>
+          </label>
+          <label className="more-label">
+            Release Date
+            <input
+              className="more-input"
+              type="text"
+              name="releaseDate"
+              value={songInfo.releaseDate}
+              onChange={handleChange}
+            ></input>
+          </label>
+          <input className="submit" type="submit" value="submit"></input>
+        </div>
       </form>
     </div>
   );
